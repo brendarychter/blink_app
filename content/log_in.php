15451e6fdee56226cc5c70f6fbe2884e1f34a303
Brenda Rychter <brendarychter@gmail.com>
2016-08-28 23:22:25 -0300

    comment old header

--- content/log_in.php
+++ content/log_in.php
@@ -26,7 +26,45 @@
     <div class="login-section">
         <?php include('header.php');?> 
     </div>
+<!--     <nav class="navbar navbar-default navbar-fixed-top navbar-login">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="../index.php" style="margin-top: -14px; padding: 0;"><img alt="Blink app" title="Blink app" src="../public/img/logo_small.svg" id="icon-top"></img></a>
+            </div>
 
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../index.php">blink!</a>
+                    </li>
+                     <li>
+                        <a class="page-scroll" href="#">Conoc&eacute; m&aacute;s</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">Demo</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">FAQ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">Qui&eacute;nes somos</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../index.php#contact">Contactate</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> -->
     <section id="login_section">
         <div class="row">
             <div class="col-lg-12">
