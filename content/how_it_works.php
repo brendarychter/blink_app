7619663f6b9917add19ac41b1bfd309370e7f47e
brychter <brychter@despegar.com>
2016-11-12 20:02:29 -0300

    all how it works section mobile without images

---
+++ content/how_it_works.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html class="no-js"> 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CAMBIAR ACÁ TAMBIÉN EL TITLE POR IDIOMA -->
+    <title>Como funciona - Blink App</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="../public/css/content.css">
+    <link rel="stylesheet" href="../public/css/bootstrap.css" >
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../public/css/styles_default.css">
+    <link rel="shortcut icon" type="image/png" href="../public/img/icon_tab.png"/>
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src="../public/js/global/modernizr.js"></script>
+    <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/reset.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/fonts.css">
+</head>
+<body id="page-top" class="index landing-main-page">
+    <!--burger button animation https://envato.com/blog/pure-css-animation-snippets/-->
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: #222; font-size: 14px">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- href="#page-top"-->
+                <a class="navbar-brand page-scroll" href="#page-top" style="margin-top: -4px; padding: 0;"><img alt="Blink app" title="Blink app" src="../public/img/logo.svg" id="icon-top"></img></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-home" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-middle central-nav">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="../index.php#conoce" target="_blank">blink!</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#conoce">Conoc&eacute; m&aacute;s</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#demo">Demo</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#faq">FAQ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#nosotros">Qui&eacute;nes somos</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contactate</a>
+                    </li>
+                    
+                </ul>
+                <ul class="nav navbar-nav navbar-right logandsign" id="logandsign">
+                    <li>
+                        <a class="button" href="content/log_in.php#login-box-link">Inici&aacute; sesi&oacute;n</a>
+                    </li>
+                    <li class="" style="border-left: 1px solid #CCC;">
+                        <a class="button" href="content/log_in.php#signup-box-link"><b>Registrate</b></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <h1>&#191;C&oacute;mo puedo usar la aplicaci&oacute;n?</h1>
+        <h2>Te presentamos una gu&iacute;a pr&aacute;ctica de pasos a seguir para descargarla y utilizarla</h2>
+    </header>
+
+    <section id="cd-timeline" class="cd-container">
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" alt="Picture">
+            </div> <!-- cd-timeline-img -->
+
+            <div class="cd-timeline-content">
+                <h3>Lector de QR</h3>
+                <p>Primero necesitas descargarte una aplicaci&oacute;n para leer el c&oacute;digo de descarga</p>
+                <a href="https://itunes.apple.com/us/app/qr-reader-for-iphone/id368494609?mt=8" class="cd-read-more" style="margin-left: 5px;" target="_blank">Itunes</a>
+                <a href="https://play.google.com/store/apps/details?id=me.scan.android.client&hl=es" class="cd-read-more" style="margin-left: 5px;" target="_blank">Android</a>
+                <span class="cd-date">Paso 1</span>
+            </div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-movie">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
+            </div> <!-- cd-timeline-img -->
+
+            <div class="cd-timeline-content">
+                <h3>Acced&eacute; al sitio</h3>
+                <p>En la p&aacute;gina de la home del sitio, en la secci&oacute;n de descargas, vas a encontrar la imagen a descargar</p>
+                <a href="www.blinkapp.com.ar" class="cd-read-more">Home</a>
+                <span class="cd-date">Paso 2</span>
+            </div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" alt="Picture">
+            </div> <!-- cd-timeline-img -->
+
+            <div class="cd-timeline-content">
+                <h3>Create una cuenta</h3>
+                <p>Necesitas acceder con un nombre de usuario y una contrase&ntilde;a. Agreg&aacute; tambi&eacute;n una imagen para que tus contactos puedan encontrarte r&aacute;pidamente</p>
+                <span class="cd-date">Paso 3</span>
+            </div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-location">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.svg" alt="Location">
+            </div> <!-- cd-timeline-img -->
+
+            <div class="cd-timeline-content">
+                <h3>Empez&aacute; a crear</h3>
+                <p>Cre&aacute; un grupo e invit&aacute; a contactos para que participen y puedan cargar contenido. Podr&aacute;n manejar calendarios, eventos, registros y mucho m&aacute;s</p>
+                <a href="#0" class="cd-read-more">Ver preguntas frecuentes</a>
+                <span class="cd-date">Paso 4</span>
+            </div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-location">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.svg" alt="Location">
+            </div> <!-- cd-timeline-img -->
+
+            <div class="cd-timeline-content">
+                <h3>Aprovech&aacute; al m&aacute;ximo las notificicaciones</h3>
+                <p>Configur&aacute; las opciones para recibir notificaciones en tiempo real cuando tus amigos interact&uacute;n con la aplicaci&oacute;n</p>
+            </div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+    </section> <!-- cd-timeline -->
+    <?php
+        include 'footer.php';
+    ?>
+
+    <!-- Scripts -->
+    
+    <!-- jQuery -->
+    <script src="../public/js/global/jquery.js"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../public/js/global/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="../public/js/global/jqueryeasing.js"></script>
+    <script src="../public/js/global/classie.js"></script>
+    <script src="../public/js/global/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="../public/js/global/jqBootstrapValidation.js"></script>
+    <script src="../public/js/global/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../public/js/global/agency.js"></script>
+
+    <!-- TIMELINE -->
+    <script src="../public/js/global/index_timeline.js"></script>
+
+
+</body>
+</html>
