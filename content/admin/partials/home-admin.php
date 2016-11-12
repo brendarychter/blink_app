00200275fecbc8b324589f3b299d41f0bc62f878
Brenda Rychter <brendarychter@gmail.com>
2016-11-12 13:49:24 -0300

    all sections html

--- content/admin/partials/home-admin.php
+++ content/admin/partials/home-admin.php
@@ -1,69 +1,9 @@
 <div class="col-lg-12 col-sm-12 col-md-12">
 	<div class="panel panel-default">
 		<div class="panel-heading"></div>
-			<div class="panel-body">
-				<label class="section-name">Secci&oacute;n 1</label>
-				<form role="form" class="form-section" id="form-section-1" method="post" enctype="multipart/form-data" data-toggle="validator">
-					<div class="col-md-12 col-sm-12 line-sections">
-						<div class="form-group col-md-3 box-section col-sm-12">
-							<div class="title-box">
-								<span class="global-title">t&iacute;tulo</span>
-							</div>
-							<div class="portion portion-spanish">
-								<label class="lan">Espa&ntilde;ol</label>
-								<span class="editable-text titulo-spanish"></span>
-							</div>
-							<div class="portion portion-english">
-								<label class="lan">Ingl&eacute;s</label>
-								<span class="editable-text titulo-english"></span>
-							</div>
-						</div>						
-						<div class="form-group col-md-3 box-section col-sm-12">
-							<div class="title-box">
-								<span class="global-title">subt&iacute;tulo</span>
-							</div>
-							<div class="portion portion-spanish">
-								<label class="lan">Espa&ntilde;ol</label>
-								<span class="editable-text"></span>
-							</div>
-							<div class="portion portion-english">
-								<label class="lan">Ingl&eacute;s</label>
-								<span class="editable-text"></span>
-							</div>
-						</div>
-						<div class="form-group col-md-3 box-section col-sm-12">
-							<div class="title-box">
-								<span class="global-title">bot&oacute;n</span>
-							</div>
-							<div class="portion portion-spanish">
-								<label class="lan">Espa&ntilde;ol</label>
-								<span class="editable-text"></span>
-							</div>
-							<div class="portion portion-english">
-								<label class="lan">Ingl&eacute;s</label>
-								<span class="editable-text"></span>
-							</div>
-						</div>
-						<div class="form-group col-md-3 box-section col-sm-12" style="border-right: none">
-							<div class="title-box">
-								<span class="global-title">imagen de fondo</span>
-							</div>
-							<input type="file">
-							<p class="help-block"></p>
-						</div>
-					</div>
-					<div class="abm-buttons">
-						<!-- <button type="submit" class="btn btn-primary edit-section" id="edit-section-1">Editar secci&oacute;n 1</button> -->
-						<button type="submit" class="btn btn-danger edit-section" id="delete-section-1">Eliminar secci&oacute;n 1</button>
-						<!-- tiene que borrar el div del sitio -->
-					</div>
-				</form>	
-
-			</div>
-		<div class="panel-heading" style="padding-top: 0;"></div>
 		<div class="panel-body">
-			<label class="section-name">Secci&oacute;n 2</label>
-			<form role="form" class="form-section" id="form-section-2">
+			<label class="section-name">Secci&oacute;n 1</label>
+			<form role="form" class="form-section" id="form-section-1" method="post" enctype="multipart/form-data" data-toggle="validator">
 				<div class="col-md-12 col-sm-12 line-sections">
 					<div class="form-group col-md-3 box-section col-sm-12">
 						<div class="title-box">
@@ -76,6 +16,66 @@
 						<div class="portion portion-english">
 							<label class="lan">Ingl&eacute;s</label>
 							<span class="editable-text titulo-english"></span>
+						</div>
+					</div>						
+					<div class="form-group col-md-3 box-section col-sm-12">
+						<div class="title-box">
+							<span class="global-title">subt&iacute;tulo</span>
+						</div>
+						<div class="portion portion-spanish">
+							<label class="lan">Espa&ntilde;ol</label>
+							<span class="editable-text"></span>
+						</div>
+						<div class="portion portion-english">
+							<label class="lan">Ingl&eacute;s</label>
+							<span class="editable-text"></span>
+						</div>
+					</div>
+					<div class="form-group col-md-3 box-section col-sm-12">
+						<div class="title-box">
+							<span class="global-title">bot&oacute;n</span>
+						</div>
+						<div class="portion portion-spanish">
+							<label class="lan">Espa&ntilde;ol</label>
+							<span class="editable-text"></span>
+						</div>
+						<div class="portion portion-english">
+							<label class="lan">Ingl&eacute;s</label>
+							<span class="editable-text"></span>
+						</div>
+					</div>
+					<div class="form-group col-md-3 box-section col-sm-12" style="border-right: none">
+						<div class="title-box">
+							<span class="global-title">imagen de fondo</span>
+						</div>
+						<input type="file">
+						<p class="help-block"></p>
+					</div>
+				</div>
+				<div class="abm-buttons">
+					<!-- <button type="submit" class="btn btn-primary edit-section" id="edit-section-1">Editar secci&oacute;n 1</button> -->
+					<button type="submit" class="btn btn-danger edit-section" id="delete-section-1">Eliminar secci&oacute;n 1</button>
+					<!-- tiene que borrar el div del sitio -->
+				</div>
+			</form>	
+
+		</div>
+		<div class="panel-heading" style="padding-top: 0;"></div>
+		<div class="panel-body">
+			<label class="section-name">Secci&oacute;n 2</label>
+			<form role="form" class="form-section" id="form-section-2">
+				<div class="col-md-12 col-sm-12 line-sections">
+					<div class="form-group col-md-3 box-section col-sm-12">
+						<div class="title-box">
+							<span class="global-title">t&iacute;tulo</span>
+						</div>
+						<div class="portion portion-spanish">
+							<label class="lan">Espa&ntilde;ol</label>
+							<span class="editable-text"></span>
+						</div>
+						<div class="portion portion-english">
+							<label class="lan">Ingl&eacute;s</label>
+							<span class="editable-text"></span>
 						</div>
 					</div>						
 					<div class="form-group col-md-3 box-section col-sm-12">
@@ -197,7 +197,7 @@
 		<div class="panel-heading"></div>
 		<div class="panel-body">
 			<label class="section-name">Secci&oacute;n 3</label>
-			<form role="form" class="form-section" id="form-section-1" method="post" enctype="multipart/form-data" data-toggle="validator">
+			<form role="form" class="form-section" id="form-section-3" method="post" enctype="multipart/form-data" data-toggle="validator">
 				<div class="col-md-12 col-sm-12 line-sections">
 					<div class="form-group col-md-3 box-section col-sm-12" style="border-right: none">
 						<div class="title-box">
@@ -213,6 +213,52 @@
 					<button type="submit" class="btn btn-danger edit-section" id="delete-section-1">Eliminar secci&oacute;n 3</button>
 				</div>
 			</form>	
+		</div>
+		<div class="panel-heading"></div>
+		<div class="panel-body">
+			<label class="section-name">Secci&oacute;n 4</label>
+			<form role="form" class="form-section" id="form-section-4" method="post" enctype="multipart/form-data" data-toggle="validator">
+				<div class="col-md-12 col-sm-12 line-sections">
+					
+					<div class="form-group col-md-3 box-section col-sm-12">
+						<div class="title-box">
+							<span class="global-title">t&iacute;tulo</span>
+						</div>
+						<div class="portion portion-spanish">
+							<label class="lan">Espa&ntilde;ol</label>
+							<span class="editable-text"></span>
+						</div>
+						<div class="portion portion-english">
+							<label class="lan">Ingl&eacute;s</label>
+							<span class="editable-text"></span>
+						</div>
+					</div>						
+					<div class="form-group col-md-3 box-section col-sm-12">
+						<div class="title-box">
+							<span class="global-title">texto</span>
+						</div>
+						<div class="portion portion-spanish">
+							<label class="lan">Espa&ntilde;ol</label>
+							<span class="editable-text"></span>
+						</div>
+						<div class="portion portion-english">
+							<label class="lan">Ingl&eacute;s</label>
+							<span class="editable-text"></span>
+						</div>
+					</div>
+					<div class="form-group col-md-3 box-section col-sm-12" style="border-right: none">
+						<div class="title-box">
+							<span class="global-title">imagen QR</span>
+						</div>
+						<input type="file">
+						<p class="help-block"></p>
+					</div>
+				</div>
+				<div class="abm-buttons">
+					<button type="submit" class="btn btn-danger edit-section" id="delete-section-1">Eliminar secci&oacute;n 4</button>
+				</div>
+			</form>	
+
 		</div>
 	</div>
 </div>
