3d728158dc912441faadc389e82ebc388d0e1c46
brychter <brychter@despegar.com>
2017-06-21 15:09:59 -0300

    progress order some things

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blink_app
 Todos tus grupos en un mismo lugar
+
+Author: Brenda Yael Rychter
--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
-<html class="no-js"> 
+<html class="no-js" lang="es"> 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Blink App - Home</title>
-    <meta name="description" content="">
+    <!-- Agregarle el idioma -->
+    <meta name="description" content="Blink app es una herramienta, un aliado en tu día a día. Es la mejor manera de mantenerte informado todo el tiempo de todo lo que pasa en tus grupos al recibir notificaciones en el momento en que tus amigos publican contenidos.">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!--CSS-->
+    <!--STYLES-->
     <link rel="stylesheet" href="public/css/bootstrap.css" >
     <link rel="stylesheet" type="text/css" href="public/css/content.css">
-
-
     <!-- Custom CSS -->
     <link rel="stylesheet" href="public/css/styles_default.css">
-    <link rel="shortcut icon" type="image/png" href="" class="img_icon_menu" />
+
+    <link rel="shortcut icon" type="image/png" href="public/img/icon_tab.png" class="img_icon_menu" />
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -84,110 +85,119 @@
             <div class="intro-text">
                 <h2 class="intro-lead-in subtitle_section_1">Una &uacute;nica aplicaci&oacute;n. Todo lo que necesit&aacute;s.</h2>
                 <h1 class="intro-heading title_section_1">Tus grupos en un mismo lugar</h1>
-                <!--<a href="#conoce" class="page-scroll btn btn-xl" style="margin-right:5px">Conoc&eacute; m&aacute;s</a>-->
                 <a href="#download-app" class="page-scroll btn btn-xl button_section_1">Descarg&aacute; la app</a>
             </div>
         </div>
     </header>
 
-    <!-- conoce Section -->
-    <section id="conoce" class="section_2 page-section-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading title_section_2">&#191;Qu&eacute; es blink?</h2>
-                    <h3 class="section-subheading text-muted subtitle_section_2" style="line-height: 28px;margin-bottom:0"> &#191;Ten&eacute;s un grupo de amigos de la facultad? &#191;Otro de la oficina? &#191;De f&uacute;tbol o baile?</br><span style="font-style: normal;">Blink te permite comunicarte con todos ellos desde un mismo lugar.</span> </br></h3>
-                    <span class="text_section_2" style="margin-top:16px; display: block; font-weight: bold; font-style: normal;">Es una herramienta, un aliado en tu d&iacute;a a d&iacute;a. Es la mejor manera de mantenerte informado todo el tiempo de todo lo que pasa en tus grupos al recibir notificaciones en el momento en que tus amigos publican contenidos.</span>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-3">
-                    <h4 class="service-heading"><img src="public/img/group.svg" /></h4>
-                    <p class="text-muted text_1_section_2">Cre&aacute; todos los grupos que necesites</p>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="service-heading"><img src="public/img/hour.svg" /></h4>
-                    <p class="text-muted text_2_section_2">Llev&aacute; un registro de los gastos grupales</p>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="service-heading"><img src="public/img/calendar.svg" /></h4>
-                    <p class="text-muted text_3_section_2">No te olvides de ning&uacute;n evento</p>
-                </div>
-                <div class="col-md-3">
-                    <h4 class="service-heading"><img src="public/img/chat.svg" /></h4>
-                    <p class="text-muted text_4_section_2">Enterate de las &uacute;ltimas novedades</p>
-                </div>
-            </div>
-            <div style="text-align: center;">
-                <a href="content/how_it_works.php" class="page-scroll btn btn-xl button_section_2" id="more-info">Todav&iacute;a quiero saber m&aacute;s</a>
-            </div>
-        </div>
-    </section>
+    <!-- Main home -->
+    <main>
 
-    <section id="demo" class="section_3 page-section-home">
-        <img src="public/img/play.svg" class="demo-img"><br><br>
-        <p class="demo_description">
-            <a href="content/demo.php" style="cursor: pointer;">Video en construcci&oacute;n</a>
-        </p>
-    </section>
-    <section id="download-app" class="section_4 page-section-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center download-application">
-                    <img src="public/img/qr_app_example.png">
-                    <h2 class="section-heading text-app title_section_4">Descargate la aplicaci&oacute;n para el celular</h2>
-                    <h3 class="section-subheading text-muted text_section_4">Vas a poder acceder a las novedades m&aacute;s r&aacute;pidamente y usar la aplicaci&oacute;n de forma m&aacute;s c&oacute;moda y eficiente</h3>
-                    <div style="text-align: center;">
-                        <a href="content/faq.php" class="page-scroll btn btn-xl button_section_4" id="more-info">¿Ten&eacute;s dudas?</a>
+        <!-- CONOCE Section -->
+        <section id="conoce" class="section_2 page-section-home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading title_section_2">&#191;Qu&eacute; es blink?</h2>
+                        <h3 class="section-subheading text-muted subtitle_section_2" style="line-height: 28px;margin-bottom:0"> &#191;Ten&eacute;s un grupo de amigos de la facultad? &#191;Otro de la oficina? &#191;De f&uacute;tbol o baile?</br><span style="font-style: normal;">Blink te permite comunicarte con todos ellos desde un mismo lugar.</span> </br></h3>
+                        <span class="text_section_2" style="margin-top:16px; display: block; font-weight: bold; font-style: normal;">Es una herramienta, un aliado en tu d&iacute;a a d&iacute;a. Es la mejor manera de mantenerte informado todo el tiempo de todo lo que pasa en tus grupos al recibir notificaciones en el momento en que tus amigos publican contenidos.</span>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-3">
+                        <h4 class="service-heading"><img src="public/img/group.svg" /></h4>
+                        <p class="text-muted text_1_section_2">Cre&aacute; todos los grupos que necesites</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="service-heading"><img src="public/img/hour.svg" /></h4>
+                        <p class="text-muted text_2_section_2">Llev&aacute; un registro de los gastos grupales</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="service-heading"><img src="public/img/calendar.svg" /></h4>
+                        <p class="text-muted text_3_section_2">No te olvides de ning&uacute;n evento</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="service-heading"><img src="public/img/chat.svg" /></h4>
+                        <p class="text-muted text_4_section_2">Enterate de las &uacute;ltimas novedades</p>
+                    </div>
+                </div>
+                <div style="text-align: center;">
+                    <a href="content/how_it_works.php" class="page-scroll btn btn-xl button_section_2" id="more-info">Todav&iacute;a quiero saber m&aacute;s</a>
+                </div>
+            </div>
+        </section>
+    
+        <!-- VIDEO Section -->
+        <section id="demo" class="section_3 page-section-home">
+            <img src="public/img/play.svg" class="demo-img"><br><br>
+            <p class="demo_description">
+                <a href="content/demo.php" style="cursor: pointer;">Video en construcci&oacute;n</a>
+            </p>
+        </section>
+    
+        <!-- DOWNLOAD APP Section -->
+        <section id="download-app" class="section_4 page-section-home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center download-application">
+                        <img src="public/img/qr_app_example.png">
+                        <h2 class="section-heading text-app title_section_4">Descargate la aplicaci&oacute;n para el celular</h2>
+                        <h3 class="section-subheading text-muted text_section_4">Vas a poder acceder a las novedades m&aacute;s r&aacute;pidamente y usar la aplicaci&oacute;n de forma m&aacute;s c&oacute;moda y eficiente</h3>
+                        <div style="text-align: center;">
+                            <a href="content/faq.php" class="page-scroll btn btn-xl button_section_4" id="more-info">¿Ten&eacute;s dudas?</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- Contact Section -->
-    <section id="contact" class="section_5 page-section-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading subtitle_section_5">Contactate</h2>
-                    <h3 class="section-subheading text-muted title_section_5">&iexcl;Queremos saber tu opini&oacute;n!</h3>
+        </section>
+
+        <!-- CONTACT Section -->
+        <section id="contact" class="section_5 page-section-home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading subtitle_section_5">Contactate</h2>
+                        <h3 class="section-subheading text-muted title_section_5">&iexcl;Queremos saber tu opini&oacute;n!</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <form name="sentMessage" id="contactForm" novalidate>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" class="text_1_section_5 form-control" placeholder="Nombre *" id="name" required data-validation-required-message="Por favor ingresa tu nombre">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="text_2_section_5 form-control" placeholder="Mail *" id="email" required data-validation-required-message="Por favor ingresa tu mail.">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="tel" class="text_3_section_5 form-control" placeholder="N&uacute;mero de tel&eacute;fono" id="phone" required data-validation-required-message="Por favor ingresa tu n&uacute;mero de tel&eacute;fono.">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <textarea class="text_4_section_5 form-control" placeholder="Tu mensaje *" id="message" required data-validation-required-message="Por favor ingresa tu mensaje."></textarea>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
+                                <div class="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button type="submit" class="button_section_5 btn btn-xl">Enviar mensaje</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <form name="sentMessage" id="contactForm" novalidate>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="text" class="text_1_section_5 form-control" placeholder="Nombre *" id="name" required data-validation-required-message="Por favor ingresa tu nombre">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="text_2_section_5 form-control" placeholder="Mail *" id="email" required data-validation-required-message="Por favor ingresa tu mail.">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="tel" class="text_3_section_5 form-control" placeholder="N&uacute;mero de tel&eacute;fono" id="phone" required data-validation-required-message="Por favor ingresa tu n&uacute;mero de tel&eacute;fono.">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <textarea class="text_4_section_5 form-control" placeholder="Tu mensaje *" id="message" required data-validation-required-message="Por favor ingresa tu mensaje."></textarea>
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="col-lg-12 text-center">
-                                <div id="success"></div>
-                                <button type="submit" class="button_section_5 btn btn-xl">Enviar mensaje</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
+
+    </main>
+    
 
     <?php
         include 'content/footer.php';
