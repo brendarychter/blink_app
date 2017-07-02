6b1f72ecd8103ae31786c838d67cb83d3b471cf0
Brenda Rychter <brendarychter@gmail.com>
2017-07-02 01:03:10 -0300

    progress footer almost finished

--- content/footer.php
+++ content/footer.php
@@ -1,56 +1,54 @@
-<footer class="footer-distributed">
-
-    <div class="footer-left">
-        <p class="footer-company-about">
-            <span style="font-size: 13px; color: #ccc">BLINK APP</span>
-            <p class="footer-links">
-                <a href="#" id="spanish" class="language-selection">Espa&ntilde;ol</a>
-                ·
-                <a href="#" id="english" class="language-selection">Ingl&eacute;s</a>
-                </br>
-                <a href="#" target="_blank">Pol&iacute;ticas de Privacidad</a>
-                ·
-                <a href="#" target="_blank">T&eacute;rminos de uso</a>
-                </br>
-                <script type="text/javascript">
-                    console.log(document.referrer)
-                </script>
-                <a href="map.php">Mapa</a>
-            </p>
-            <span class="newsletter-title" style="font-size: 13px; color: #ccc">SUSCRIBITE AL NEWSLETTER</span>
-            <div class="newsletter">
-                <script type="text/javascript" src="http://v2.envialosimple.com/form/show/AdministratorID/93288/FormID/1/format/widget"></script>
-            </div>
+<footer class="footer-blink">
+    <div class="footer-box">
+        <span class="footer-box-title">Blink app</span>
+        <p class="footer-links">
+            <a href="#" id="spanish" class="language-selection">Espa&ntilde;ol</a>
+            ·
+            <a href="#" id="english" class="language-selection">Ingl&eacute;s</a>
+            </br>
         </p>
-        
+        <p class="footer-links">
+            <a href="#" target="_blank">Pol&iacute;ticas de Privacidad</a>
+            </br>
+            <a href="#" target="_blank">T&eacute;rminos de uso</a>
+            </br>
+            <a href="content/map.php">Mapa</a>
+        </p>
+        <div class="separator">·</div>
     </div>
-    <div class="footer-right">
-
-        <h3>Brenda Rychter</h3>
-        <a href="http://www.brendarychter.com.ar" target="_blank">www.brendarychter.com.ar</a>
-        
-        <p class="footer-company-name">Brenda Rychter &copy; 2016</p>
-
+ 
+    <div class=" footer-box newsletter-container">
+        <span class="footer-box-title">Newsletter</span>
+        <div class="newsletter">
+            <script type="text/javascript" src="http://v2.envialosimple.com/form/show/AdministratorID/93288/FormID/1/format/widget"></script>
+        </div>
+        <div class="separator">·</div>
     </div>
 
-    <div class="footer-center">
-
-        <div>
-            <i class="fa fa-map-marker"></i>
+    <div class="footer-box">
+        <span class="footer-box-title">Seguinos en las redes</span>
+        <ul class="list-inline social-buttons">
+            <li><a href="www.facebook.com" target="_blank"><img src="../public/img/icons/fb.svg"></a>
+            <li><a href="www.facebook.com" target="_blank"><img src="../public/img/icons/fb.svg"></a>
+            <li><a href="www.facebook.com" target="_blank"><img src="../public/img/icons/fb.svg"></a>
+        </ul>
+        <div class="separator">·</div>
+    </div>
+ 
+    <div class="footer-box">
+        <span class="footer-box-title">Desarrollado por</span>
+        <div class="dev-info">
+            <h5 class="dev-name">Brenda Yael Rychter</h5>
+            <a href="http://www.brendarychter.com.ar" target="_blank" class="web-dev">www.brendarychter.com.ar</a> 
+            <ul class="list-inline social-buttons">
+                <li><a href="www.facebook.com" target="_blank"><img src="../public/img/icons/fb.svg"></a>
+                <!-- <p>+1565220958</p>  -->
+                <li><a href="mailto:brendarychter@gmail.com" target="_blank"><img src="../public/img/icons/fb.svg"></a>
+            </ul>
             <p><span>Capital Federal</span> Buenos Aires, Argentina</p>
         </div>
-
-        <div>
-            <i class="fa fa-phone"></i>
-            <p>+1565220958</p>
-        </div>
-
-        <div>
-            <i class="fa fa-envelope"></i>
-            <p><a href="mailto:support@company.com">brendarychter@gmail.com</a></p>
-        </div>
+        
 
     </div>
-
 
 </footer>
